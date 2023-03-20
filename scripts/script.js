932535ca27bd6d33415ff3fbffddce08a9234d6d
Katy Chan <cchan@my.bcit.ca>
2023-03-20 10:43:02 -0700

    textcontent practice

--- scripts/script.js
+++ scripts/script.js
@@ -50,10 +50,65 @@
 
 // 2 var and default value
 
-function greet(name, time="day"){
-    console.log(`Good ${time} ${name}`)
+// function greet(name, time="day"){
+//     console.log(`Good ${time} ${name}`)
     
-  }
+//   }
 
-  greet("kk", "night")
-  greet("kk")
+//   greet("kk", "night")
+//   greet("kk")
+
+
+
+
+
+
+
+// calculate
+const enteredValue = prompt("enter a number: ")
+
+//   check if value is not a numberlet enteredValue;
+
+// do{
+//     enteredValue = prompt("enter a number: ");
+//  }while(isNaN(enteredValue))
+
+
+// references to p tags
+
+const radiuspara = document.querySelector("#radius")
+radiuspara.textContent = enteredValue
+console.log(radiuspara)
+const resultpara = document.querySelector("#result")
+console.log(resultpara)
+
+
+function calculateArea (radius){
+
+//   check if value is not a numberlet enteredValue;
+
+  if(isNaN(radius)){
+    alert("This is not a number");
+    // update the resultpara to show the error
+    resultpara.textContent = "This is not a number"
+  }else{
+  
+  const area = Math.PI*radius*radius;
+  
+  return area.toFixed(2);
+  
+ }
+  
+}
+
+let result = calculateArea(enteredValue);
+
+// alert(`The area of a circle with radius ${enteredValue} is ${result}`)
+
+if (!isNaN(enteredValue)){
+resultpara.textContent = `The area of a circle with radius ${enteredValue} is ${result}`
+}
+
+// // showing text
+// resultpara.textContent
+// output ---> 'Show area or Error'
